dae9cba7b593559f3d5f6792272cf26d7575b792
hernandezn <hernandezn@gatech.edu>
2025-01-07 01:41:44 -0500

    Added Anonymizer Script & Fixed WIP Entry Labels
    Anonymizer script is to be able to show the design of the page without the text, and the WIP page had the Career Index and WIP buttons swapped.

---
+++ public/scripts/anonymize.js
@@ -0,0 +1,33 @@
+
+const anonymize = () => {
+    const getTextNodes = (el) => {
+        const children = [] // Type: Node[]
+        const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT)
+        while(walker.nextNode()) {
+        children.push(walker.currentNode)
+        }
+        return children
+    }
+
+    const nodes = getTextNodes(document.body);
+
+    for(const node of nodes) {
+        let str = '';
+        const content = node.textContent;
+        const len = content.length;
+        
+        for(let i = 0; i < len; i++) {
+            let addend;
+
+            if(/\s/.test(content[i])) {
+                addend = content[i];
+            } else {
+                addend = '#';
+            }
+
+            str = str + addend;
+        }
+
+        node.textContent = str;
+    }
+}
