94360e070620358a10892465d190485687725a4e
hernandezn <hernandezn@live.com>
2025-01-20 22:29:09 -0500

    Eloquent JavaScript 2024 Ch 5 Exercises Reduction
    The latter exercise demonstrations from chapter 5 are replaced by the code I used to solve them, which can just be pasted onto the Eloquent JavaScript site for demonstration.

--- public/professional/self-development/eloquent-javascript-2024/chapter-5-exercises.js
+++ public/professional/self-development/eloquent-javascript-2024/chapter-5-exercises.js
@@ -37,69 +37,32 @@
 /*
     Exercise 5.2 - Your Own Loop
 
-    THIS IS BEING DISABLED. It's arbitrary code execution; 
-    can't be having it run from user input on the live page, not when I plan 
-    to have APIs that allow page inputs from this origin.
+    USER INPUT AND PAGE OUTPUT WILL NOT BE ENABLED FOR THIS. It's arbitrary 
+    code execution; can't be having it run from user input on the live page, 
+    not when I plan to make this a dynamic site where the origin environment 
+    can matter. Instead, this will run on the textbook site.
 */
 
-// {
-//     const input1 = document.getElementById('exercise-5-2-input-1');
-//     const input2 = document.getElementById('exercise-5-2-input-2');
-//     const input3 = document.getElementById('exercise-5-2-input-3');
-//     const input4 = document.getElementById('exercise-5-2-input-4');
-//     const output1 = document.getElementById('exercise-5-2');
+{
+    const loop = (value, test, update, body) => {
+        let i = value;
 
-//     const loop = (value, test, update, body) => {
-//         let i = value;
+        while(test(i)) {
+            body(i);
+            i = update(i);
 
-//         while(test(i)) {
-//             body(i);
-//             i = update(i);
+            console.log(i);
+            console.log(output1.innerText);
+        }
 
-//             console.log(i);
-//             console.log(output1.innerText);
-//         }
+        return i;
+    };
 
-//         return i;
-//     };
-
-//     // everything except the one line is just to take input from the user and render output to the page...
-//     const doExercise = () => {
-//         let [value, test, update, body] = [null, null, null, null];
-        
-//         try {
-//             // [value, test, update, body] = 
-//             // [
-//             //     eval(input1.value),
-//             //     eval(input2.value),
-//             //     eval(input3.value),
-//             //     eval(input4.value),
-//             // ]
-
-//             output1.innerText = '321';
-//             // loop(value, test, update, body);
-
-//         } catch(e) {
-//             output1.innerText = e;
-
-//             return;
-//         }
-//     };
-
-//     input1.addEventListener('change', doExercise);
-//     input1.addEventListener('keyup', doExercise);
-
-//     input2.addEventListener('change', doExercise);
-//     input2.addEventListener('keyup', doExercise);
-
-//     input3.addEventListener('change', doExercise);
-//     input3.addEventListener('keyup', doExercise);
-
-//     input4.addEventListener('change', doExercise);
-//     input4.addEventListener('keyup', doExercise);
-
-//     doExercise();
-// };
+    loop(3, n => n > 0, n => n - 1, console.log);
+    // → 3
+    // → 2
+    // → 1
+};
 
 /* 
     Exercise 5.3 - Everything
